84537461501add2599512355595e7732b460bcbe
cami65 <tarverdiyevkamilinho2@gmail.com>
2022-11-26 18:26:30 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.scss';
-import {HomePage} from './pages/HomePage/HomePage';
 import {Header} from './pages/Header/Header'
 function App() {
   return (
     <div className="App">
       <Header/>
       <main>
-        <HomePage />
+        < />
       </main>
     </div>
   );
--- src/pages/HomePage/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-
-
-import './HomePage.scss'
-
-export const HomePage =() => {
-    return(
-<h1>Besnik Agency</h1>
-    );
-};
